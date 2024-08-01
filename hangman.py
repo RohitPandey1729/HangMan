b8b71eae57a453bf50ccece57a4b8749871895e6
Rohit Pandey <myboxrohit@gmail.com>
2024-08-01 10:58:23 -0700

    I started my project and added some code

---
+++ hangman.py
@@ -0,0 +1,22 @@
+
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    lives = 7
